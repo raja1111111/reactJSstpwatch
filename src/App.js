e5fbf4bd5e61b44f051902ea008ee1961dbc0e9d
raja1111111 <raja2abdallah98@gmail.com>
2020-11-12 12:39:20 -0800

    creat stopwatch react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
+
+import React from 'react'
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor (props){
+    
+    super(props);
+   this.Timer=Timer
+    this.state= {
+      hour :0,
+      minute:0,
+      second:0
+
+    }
+  }
+
+  handlestart = () => {
+
+    this.timer = setInterval(()=>{
+      this.setState((prevState) =>{
+        return{
+          second :prevState.second +1
+        };
+      });
+
+      if(this.state.second /60==1){
+        this.setState((prevState)=>{
+          return{
+            minute : prevState.minute +1,
+            second :0
+          }
+        })
+      }
+      if(this.state.minute /60===1){
+        this.setState((prevState)=>{
+          return{
+            hour :prevState.hour +1,
+            minute :0
+          }
+
+        })
+      }
+    },1000);
+    // this.setState((prevState) =>{
+    //   return{
+    //     hour : prevState.hour + 1,
+    //     minute:prevState.minute + 1,
+    //     second:prevState.second + 1
+    //   }
+
+    
+  };
+
+  handlestop = () => {
+    clearInterval(this.timer);
+    
+      }
+
+      handlereset = () => {
+        this.setState(()=>{
+          return {
+            hour :0,
+            minute :0,
+            second :0
+          }
+        })
+          }
+  render(){
+    return(
+      <div className="container">
+        <Header />
+        <Timer time={this.state}/>
+        <Action handlestart ={this.handlestart} handlestop={this.handlestop}  handlereset={this.handlereset}/>
+        </div>
+    );
+  }
+
+}
+
+const Header = ()=>{
+  return <h1 id="stopwatch">stop watch</h1>
+}
+
+const Timer = (props)=>{
+return (
+  <div id="display">
+    <div>{props.time.hour}:{props.time.minute}:{props.time.second}</div>
+    
+
+  </div>
+)
+}
+
+const Action = (props) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="btn">
+      <button onClick={props.handlereset}id="reset">Reset</button>
+      <button onClick={props.handlestart}id="startstop">Start</button>
+      <button onClick={props.handlestop}id="stop">stop</button>
     </div>
   );
 }
-
 export default App;
+
+
+
